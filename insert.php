2a2339872510a51582ff777d797db6a52cbe1983
Pedro Reis <olavyo.p.lopes@gmail.com>
2021-02-06 19:09:26 -0300

    pagina de com o codigo que inseri os dados no banco de dados

---
+++ insert.php
@@ -0,0 +1,36 @@
+<<?php 
+include("conexao.php");
+
+$dad= array ($_POST['nome'],$_POST['musica']);
+
+$dir="img/";
+
+$file=$_FILES['foto'];
+
+$destino="$dir".$file["name"];
+
+move_uploaded_file($file['tmp_name'], $destino);
+
+
+function inserir($conexao, $dad, $destino){
+	
+	$script = 'INSERT INTO concorrentes VALUES (DEFAULT, "' . $dad[0] . '", "' . $dad[1] . '", "' . $destino . '", 0)';
+
+	$insere = $conexao->query($script);
+	if(!$insere){
+		echo "<br>Inserção incorreta! Deu erro!<br>";
+		echo $conexao->error;
+		echo "<br><br>";
+		echo $script;
+		echo "<br>";
+	}else{
+		echo "<br>Inserção Realizada corretamente!";
+	}
+}
+
+inserir($conexao,$dad,$destino);
+
+header('Location: /ProjectWeb/index.php');
+
+
+ ?>
