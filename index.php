08c1ce05566d0fc67aa4c1778c5fa41df9b918bd
Pedro Reis <olavyo.p.lopes@gmail.com>
2021-02-06 19:06:48 -0300

    adicionado button listar dados e mudanca no visual do formulario

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <<?php 
 include("conexao.php");
 
- ?>
+?>
 
 <!DOCTYPE html>
 <html>
@@ -13,24 +13,25 @@ include("conexao.php");
 	<title>Projeto Final</title>
 </head>
 <body>
-	<h1 class="col-12">Cadastro dos concorrentes</h1><br>
-	<div class="container col-12 divcadastro rounded border border-primary mb-5"> 
-		<form class="needs-validation m-4" novalidate>
-			<div class="form-row mb-3 mt-3">
-				<label form="labelNome" class="mb-0">Nome</label> 
-				<input type="text" class="form-control input rounded-0" placeholder="Digite o Nome do concorrente"  name="nome" required>
-			</div>
-			<div class="form-row">
-				<label form="labelMusicaR" class="mb-0">Música de Referência</label>
-				<input type="text" class="form-control input rounded-0" placeholder="Digite uma música de referência" name="cpf" required>	
-			</div>
-			<div class="form-group">
-				<label form="labelFoto">Foto do concorrente</label>
-				<input type="file" class="form-control-file input" name="foto" id="foto1" required>
-			</div>
-			<input type="submit" value="Cadastrar" id="cadastro" class="btn btn-primary btn-sm btn-rounded font-weight-bold mb-2 mt-2 w-100">
+	<div class="container col-12 divcadastro rounded border border-primary mb-5">
+		<form name="tTeste" action="insert.php" method="POST" enctype="multipart/form-data">
 
-		</form>
-	</div>    
+			<label class="mb-0">Nome:</label> 
+			<input type="text" class="form-control input rounded-0" placeholder="Digite o Nome do concorrente"  name="nome" required="">
+
+
+			<label class="mb-0">Música de Referência:</label>
+			<input type="text" class="form-control input rounded-0" placeholder="Digite uma música de referência" name="musica" required="">	
+
+
+			<label form="labelFoto">Foto do concorrente:</label>
+			<input type="file" class="form-control-file input" name="foto" required="">
+
+			<input type="submit" value="Cadastrar" id="cadastro" class="btn btn-primary btn-sm btn-rounded font-weight-bold mb-2 mt-2 ">
+
+		</form> 
+		
+	</div>
+	<a href="listar.php"><button class="btn btn-primary btn-sm btn-rounded font-weight-bold ">Listar Concorrentes</button></a>
 </body>
 </html>
