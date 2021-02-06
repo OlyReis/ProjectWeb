2b80f64a5d68dc5e864dddde1c591f21e7cb9df2
Pedro Reis <olavyo.p.lopes@gmail.com>
2021-02-06 19:13:37 -0300

    apresenta so o codigo de conexao ao banco de dados

--- conexao.php
+++ conexao.php
@@ -8,7 +8,6 @@ $bd="music";
 
 $conexao= mysqli_connect($servidor, $usuario,$senha,$bd);
 
-$dad= array ($_POST['nome'],$_POST['musica'],$_POST['foto']);
 
 if (!$conexao){
 	echo"Falha na conexão!!!";
@@ -16,23 +15,7 @@ if (!$conexao){
 	echo"Banco de dados conectado com sucesso.=)";
 }
 
-function inserir($conexao, $dad){
-	
-	$script = 'INSERT INTO concorrentes VALUES (DEFAULT, "' . $dad[0] . '", "' . $dad[1] . '", "' . $dad[2] . '", 0)';
 
-	$insere = $conexao->query($script);
-	if(!$insere){
-		echo "<br>Inserção incorreta! Deu erro!<br>";
-		echo $conexao->error;
-		echo "<br><br>";
-		echo $script;
-		echo "<br>";
-	}else{
-		echo "<br>Inserção Realizada corretamente!";
-	}
-}
-
-inserir($conexao,$dad);
 
 
 
