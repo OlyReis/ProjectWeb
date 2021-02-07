e6c9bbc3a9abc16fb49c272bab1e7a52e5ce49e4
christhian12rv <christhian11rv@hotmail.com>
2021-02-06 22:21:19 -0300

    Edicao de imagens, ícone e texto dos lugares

--- vencedor.php
+++ vencedor.php
@@ -12,25 +12,36 @@ include("conexao.php");
   <title>Resultado - Big Rapper Brasil</title>
 </head>
 <body>
-  <h1 align="center" class="mb-4">Big Rapper Brasil - Resultado</h1>
-  <div class="container divResultado mb-5 border border-primary h-100 w-100" style="width: 3000px;">
-    <div class="card-columns">
-      <div class="card" style="width: 18rem;">
+  <h1 align="center" class="mb-4 mt-4">Big Rapper Brasil - Resultado</h1>
+  <div class="container divResultado mb-4 mt-4 h-100 w-75">
+    <div class="row mb-4 mt-4">
+      <div class="col" style="margin-left: 130px; margin-top: 100px;">
+        <h4 class="border rounded-circle" style="padding: 6px;  margin-right: 100px; background-color: #DCDCDC;" align="center">2º</h4>
+      </div>
+      <div class="col" >
+        <img src="img/crown icon.png" style="width: 100px; margin-left: 150px; margin-top: 40px;">
+      </div>
+      <div class="col" style="margin-left: 230px; margin-top: 100px;">
+        <h4 class="border rounded-circle" style="padding: 6px; margin-right: 100px; background-color: #DCDCDC" align="center">3º</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="card ml-4" style="width: 18rem;">
         <img class="card-img-top" src="img/294430.jpg" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title" align="center">Autor Foda</h5>
           <p class="card-text">Música de Referência: Música Foda</p>
         </div>
       </div>   
-      <div class="card" style="width: 24rem;">
-        <img class="card-img-top" src="img/294430.jpg" alt="Card image cap">
+      <div class="card ml-4 border-primary" style="width: 24rem;">
+        <img class="card-img-top" src="img/terra.jpg" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title" align="center">Autor Foda</h5>
           <p class="card-text">Música de Referência: Música Foda</p>
         </div>
       </div>     
-      <div class="card" style="width: 15rem;">
-        <img class="card-img-top" src="img/294430.jpg" alt="Card image cap">
+      <div class="card ml-4" style="width: 15rem;">
+        <img class="card-img-top" src="img/tigre.jpg" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title" align="center">Autor Foda</h5>
           <p class="card-text">Música de Referência: Música Foda</p>
