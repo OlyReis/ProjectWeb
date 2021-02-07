dff637cf44ed79c0bed0332362a1b5d72f636249
christhian12rv <christhian11rv@hotmail.com>
2021-02-06 23:33:23 -0300

    Nome do botão mudado para Votação e Adicionamento do botão Ver vencedores

--- index.php
+++ index.php
@@ -36,6 +36,7 @@ include("conexao.php");
 	<a href="listar.php"><button class="btn btn-primary btn-sm btn-rounded font-weight-bold ">Listar Concorrentes</button></a>
 	<a href="votacao.php"><button class="btn btn-primary btn-sm btn-rounded font-weight-bold">Editar Concorrente</button></a>
 	<a href="votacao.php"><button class="btn btn-primary btn-sm btn-rounded font-weight-bold">Excluir Concorrente</button></a>
-	<a href="votacao.php"><button class="btn btn-primary btn-sm btn-rounded font-weight-bold ml-4">Iniciar Votação</button></a>
+	<a href="votacao.php"><button class="btn btn-primary btn-sm btn-rounded font-weight-bold ml-4">Votação</button></a>
+	<a href="votacao.php"><button class="btn btn-primary btn-sm btn-rounded font-weight-bold ml-0">Ver vencedores</button></a>
 </body>
 </html>
