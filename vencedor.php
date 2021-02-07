a971ed601a01febe741c462f227b78fe97932701
christhian12rv <christhian11rv@hotmail.com>
2021-02-06 21:10:48 -0300

    criação da página vencedor.php

---
+++ vencedor.php
@@ -0,0 +1,43 @@
+<?php 
+include("conexao.php");
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="CSS/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="CSS/my.css">
+  <title>Resultado - Big Rapper Brasil</title>
+</head>
+<body>
+  <h1 align="center" class="mb-4">Big Rapper Brasil - Resultado</h1>
+  <div class="container divResultado mb-5 border border-primary h-100 w-100" style="width: 3000px;">
+    <div class="card-columns">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="img/294430.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title" align="center">Autor Foda</h5>
+          <p class="card-text">Música de Referência: Música Foda</p>
+        </div>
+      </div>   
+      <div class="card" style="width: 24rem;">
+        <img class="card-img-top" src="img/294430.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title" align="center">Autor Foda</h5>
+          <p class="card-text">Música de Referência: Música Foda</p>
+        </div>
+      </div>     
+      <div class="card" style="width: 15rem;">
+        <img class="card-img-top" src="img/294430.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title" align="center">Autor Foda</h5>
+          <p class="card-text">Música de Referência: Música Foda</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
