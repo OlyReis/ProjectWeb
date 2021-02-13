586f8eb8389575f870cb0df457b1e4dfdd9d7344
christhian12rv <christhian11rv@hotmail.com>
2021-02-13 17:46:13 -0300

    Ajustes e criação da sidebar e navbar

--- vencedor.php
+++ vencedor.php
@@ -9,112 +9,166 @@ include("conexao.php");
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <link rel="stylesheet" type="text/css" href="CSS/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="CSS/CirculoVotos.css">
+  <link rel="stylesheet" type="text/css" href="CSS/sidebar.css">
   <title>Resultado - Big Rapper Brasil</title>
 </head>
 <body>
-  <h1 align="center" class="mb-4 mt-4">Big Rapper Brasil - Resultado</h1>
-  <div class="container divResultado mb-4 mt-4 w-75" >
-    <div class="row mb-4 mt-4">
-      <div class="col" style="margin-left: 130px; margin-top: 100px;">
-        <h4 class="border rounded-circle" style="padding: 6px;  margin-right: 100px; background-color: #DCDCDC;" align="center">2º</h4>
-      </div>
-      <div class="col" >
-        <img src="img/crownicon.png" style="width: 100px; margin-left: 150px; margin-top: 40px;">
-      </div>
-      <div class="col" style="margin-left: 230px; margin-top: 100px;">
-        <h4 class="border rounded-circle" style="padding: 6px; margin-right: 100px; background-color: #DCDCDC" align="center">3º</h4>
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <div id="sidebar-wrapper">
+      <ul class="sidebar-nav">
+        <li class="sidebar-brand border-bottom border-primary">
+          <a href="#">
+            Big Rapper Brasil
+          </a>
+        </li>
+        <hr>
+        <li>
+          <a href="index.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
+            <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/>
+          </svg> Cadastrar Concorrentes</a>
+        </li>
+        <li>
+          <a href="listar.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-list" viewBox="0 0 16 16">
+            <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+            <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+          </svg> Listar Concorrentes</a>
+        </li>
+        <li>
+          <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+          </svg> Editar Concorrentes</a>
+        </li>
+        <li>
+          <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16" style="margin-left: -6px;">
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+          </svg>Excluir Concorrentes</a>
+        </li>
+        <li>
+          <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-square" viewBox="0 0 16 16">
+            <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z"/>
+            <path d="M8.354 10.354l7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
+          </svg> Votação</a>
+        </li>
+        <li>
+          <a href="vencedor.php"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" style="margin-top: -2px;"><path d="M3 16l-3-10 7.104 4 4.896-8 4.896 8 7.104-4-3 10h-18zm0 2v4h18v-4h-18z"/></svg> Ver vencedores</a>
+        </li>
+      </ul>
+    </div>
+
+    <div id="page-content-wrapper">
+      <div class="container-fluid">
+        <div class="nav border-bottom" style="margin: -15px 0 0 -35px; width: auto;">
+          <a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><div class="div-menu-toggle">
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+          </div></a>
+        </div>
+        <h1 align="center" class="mt-4" style="margin-bottom: 50px;">Big Rapper Brasil - Resultado</h1>
+        <div class="row-mb-5" style="display: inline-flex;">
+          <div class="col-sm-4">
+            <h5 class="text-center" style="font-size: 76px; margin-left: 10%;">2º</h5>
+            <div class="card">
+              <img class="card-img-top" src="img/294430.jpg" style="width: 100%; height: 210px;">
+              <div class="card-body">
+                <h4 class="card-title mb-4" align="center"> Nome</h4>
+                <p class="card-text">Música de Referência: Música Foda</p>
+                <p align="center">Porcentagem de votos</p>
+                <div class="progress mx-auto" data-value='31'>
+                  <span class="progress-left">
+                    <span class="progress-bar border-primary"></span>
+                  </span>
+                  <span class="progress-right">
+                    <span class="progress-bar border-primary"></span>
+                  </span>
+                  <div class="progress-value w-100 h-100 rounded-circle d-flex align-items-center justify-content-center">
+                    <div class="h2 font-weight-bold">31<sup class="small">%</sup></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" style="display: block; margin-left: auto; margin-right: auto; width: 50%; margin-top: -10px; margin-bottom: 10px;"><path d="M3 16l-3-10 7.104 4 4.896-8 4.896 8 7.104-4-3 10h-18zm0 2v4h18v-4h-18z"/></svg>
+            <div class="card border border-primary">
+              <img class="card-img-top" src="img/terra.jpg"style="width: 100%; height: 250px;">
+              <div class="card-body">
+                <h4 class="card-title mb-4" align="center"> Nome</h4>
+                <p class="card-text">Música de Referência: Música Foda</p>
+                <p align="center">Porcentagem de votos</p>
+                <div class="progress mx-auto" data-value='38'>
+                  <span class="progress-left">
+                    <span class="progress-bar border-primary"></span>
+                  </span>
+                  <span class="progress-right">
+                    <span class="progress-bar border-primary"></span>
+                  </span>
+                  <div class="progress-value w-100 h-100 rounded-circle d-flex align-items-center justify-content-center">
+                    <div class="h2 font-weight-bold">38<sup class="small">%</sup></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-3">
+            <h5 class="text-center" style="font-size: 76px; margin-left: 10%;">3º</h5>
+            <div class="card">
+              <img class="card-img-top" src="img/tigre.jpg" style="width: 100%; height: 170px;">
+              <div class="card-body">
+                <h4 class="card-title mb-4" align="center"> Nome</h4>
+                <p class="card-text">Música de Referência: Música Foda</p>
+                <p align="center">Porcentagem de votos</p>
+                <div class="progress mx-auto" data-value='24'>
+                  <span class="progress-left">
+                    <span class="progress-bar border-primary"></span>
+                  </span>
+                  <span class="progress-right">
+                    <span class="progress-bar border-primary"></span>
+                  </span>
+                  <div class="progress-value w-100 h-100 rounded-circle d-flex align-items-center justify-content-center">
+                    <div class="h2 font-weight-bold">24<sup class="small">%</sup></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="row">
-      <div class="card ml-4" style="width: 19rem;">
-        <img class="card-img-top" src="img/294430.jpg" alt="Card image cap" id="imagem1" onmouseover="aparecePainel()" onmouseleave="retiraPainel()">
-        <div class="card-body">
-          <h4 class="card-title" align="center">Autor Foda</h4>
-          <p class="card-text">Música de Referência: Música Foda</p>
-          <p align="center">Porcentagem de votos</p>
-          <div class="progress mx-auto" data-value='23'>
-            <span class="progress-left">
-              <span class="progress-bar border-primary"></span>
-            </span>
-            <span class="progress-right">
-              <span class="progress-bar border-primary"></span>
-            </span>
-            <div class="progress-value w-100 h-100 rounded-circle d-flex align-items-center justify-content-center">
-              <div class="h2 font-weight-bold">23<sup class="small">%</sup></div>
-            </div>
-          </div>
-        </div>
-      </div>   
 
-      <div class="card ml-4 border-primary" style="width: 23rem;">
-        <img class="card-img-top" src="img/terra.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h4 class="card-title" align="center">Autor Foda</h4>
-          <p class="card-text">Música de Referência: Música Foda</p>
-          <p align="center">Porcentagem de votos</p>
-          <div class="progress mx-auto" data-value='38'>
-            <span class="progress-left">
-              <span class="progress-bar border-primary"></span>
-            </span>
-            <span class="progress-right">
-              <span class="progress-bar border-primary"></span>
-            </span>
-            <div class="progress-value w-100 h-100 rounded-circle d-flex align-items-center justify-content-center">
-              <div class="h2 font-weight-bold">38<sup class="small">%</sup></div>
-            </div>
-          </div>
-        </div>
-      </div>    
+    <script type="text/javascript">
+      $(".progress").each(function() {
 
-      <div class="card ml-4" style="width: 15rem;">
-        <img class="card-img-top" src="img/tigre.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h4 class="card-title" align="center">Autor Foda</h4>
-          <p class="card-text">Música de Referência: Música Foda</p>
-          <p align="center">Porcentagem de votos</p>
-          <div class="progress mx-auto" data-value='17'>
-            <span class="progress-left">
-              <span class="progress-bar border-primary"></span>
-            </span>
-            <span class="progress-right">
-              <span class="progress-bar border-primary"></span>
-            </span>
-            <div class="progress-value w-100 h-100 rounded-circle d-flex align-items-center justify-content-center">
-              <div class="h2 font-weight-bold">17<sup class="small">%</sup></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<a href="index.php"><button class="btn btn-primary btn-sm btn-rounded font-weight-bold" style="margin-left: 200px; margin-top: 50px; margin-bottom: 50px;">Cadastrar Concorrentes</button></a>
-<a href="listar.php"><button class="btn btn-primary btn-sm btn-rounded font-weight-bold" style="margin-left: 10px; margin-top: 50px; margin-bottom: 50px;">Listar Concorrentes</button></a>
-<a href="votacao.php"><button class="btn btn-primary btn-sm btn-rounded font-weight-bold" style="margin-left: 10px;">Votação</button></a>
+        var value = $(this).attr('data-value');
+        var left = $(this).find('.progress-left .progress-bar');
+        var right = $(this).find('.progress-right .progress-bar');
 
-<script type="text/javascript">
-  $(".progress").each(function() {
+        if (value > 0) {
+          if (value <= 50) {
+            right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
+          } else {
+            right.css('transform', 'rotate(180deg)')
+            left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+          }
+        }
 
-    var value = $(this).attr('data-value');
-    var left = $(this).find('.progress-left .progress-bar');
-    var right = $(this).find('.progress-right .progress-bar');
+      })
 
-    if (value > 0) {
-      if (value <= 50) {
-        right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
-      } else {
-        right.css('transform', 'rotate(180deg)')
-        left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+      function percentageToDegrees(percentage) {
+
+        return percentage / 100 * 360
+
       }
-    }
 
-  })
-
-  function percentageToDegrees(percentage) {
-
-    return percentage / 100 * 360
-
-  }
-</script>
-</body>
-</html>
+      $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+        this.classList.toggle("change");
+      });
+    </script>
+  </body>
+  </html>
