c9ab6d2e820c030610b4b5941486fe24886fc986
christhian12rv <christhian11rv@hotmail.com>
2021-02-14 11:58:30 -0300

    Alteração do link da sidebar

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include("conexao.php");
 	<link rel="stylesheet" type="text/css" href="CSS/my.css">
 	<link rel="stylesheet" type="text/css" href="CSS/sidebar.css">
 	<meta charset="utf-8">
-	<title>Big Rapper Brasil</title>
+	<title>Cadastro de Concorrentes - Big Rapper Brasil</title>
 </head>
 <body>
 	<div id="wrapper">
@@ -37,7 +37,7 @@ include("conexao.php");
 					</svg> Listar Concorrentes</a>
 				</li>
 				<li>
-					<a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+					<a href="editar.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
 						<path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
 					</svg> Editar Concorrentes</a>
 				</li>
@@ -66,7 +66,7 @@ include("conexao.php");
 						<div class="bar3"></div>
 					</div></a>
 				</div>
-				<h1 align="center" class="mb-4 mt-4">Big Rapper Brasil</h1>
+				<h1 align="center" class="mb-4 mt-4">Cadastro de Concorrentes</h1>
 				<div class="container col-12 divcadastro rounded border border-primary mb-5">
 					<form name="tTeste" action="insert.php" method="POST" enctype="multipart/form-data" class="ml-2 mr-2">
 
